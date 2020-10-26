56107c0edee82e08a6cea1f8b9a6bfe85f327c6f
Santiago Acuña <snt-26@hotmail.com>
2020-10-26 11:30:38 -0500

    Update and Delete product for the template

--- Client-Rest/src/main/java/co/unicauca/client/ClienteMain.java
+++ Client-Rest/src/main/java/co/unicauca/client/ClienteMain.java
@@ -18,11 +18,21 @@ public class ClienteMain {
         ProductJerseyClient jersey = new ProductJerseyClient();
         Response rta = jersey.create_JSON(new Product(53, "Nevera Lg", 4000000d));
         System.out.println("Rta " + rta);
+
         // BUSCANDO UN PRODUCTO
-        Product product = jersey.findById_JSON(Product.class, "1");
+        Product product = jersey.findById_JSON(Product.class, "53");
         System.out.println(product.getId());
         System.out.println(product.getName());
         System.out.println(product.getPrice());
-        // LISTANDO PRODUCTOS
+
+        // MODIFICANDO PRODUCTO
+        product.setName("Licuadora inteligente");
+        product.setPrice(350000d);
+        rta = jersey.edit_JSON(product, "53");
+        System.out.println("Rta " + rta);
+
+        // BORRANDO PRODUCTO
+        rta = jersey.delete("53");
+        System.out.println("Rta " + rta);
     }
 }
