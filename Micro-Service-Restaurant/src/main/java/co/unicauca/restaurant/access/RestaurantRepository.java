d187607ab6602512f0dde8c4a4b38eb1c11a0bdd
Alejo Rodriguez <alejozeugirdor@unicauca.edu.co>
2020-11-15 17:23:49 -0500

    restaurant service and commons fix

--- Micro-Service-Restaurant/src/main/java/co/unicauca/restaurant/access/RestaurantRepository.java
+++ Micro-Service-Restaurant/src/main/java/co/unicauca/restaurant/access/RestaurantRepository.java
@@ -1,6 +1,7 @@
 package co.unicauca.restaurant.access;
 
-import co.unicauca.common.domain.entity.User;
+
+import co.unicauca.common.domain.entity.Restaurant;
 import co.unicauca.restaurant.infra.Utilities;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,112 +18,80 @@ import java.util.logging.Logger;
  * Es una implementación que tiene libertad de hacer una implementación del
  * contrato. Lo puede hacer con Sqlite, postgres, mysql, u otra tecnología
  *
- * @author Camilo Otaya Bravo
+ * @author Camilo Otaya
  */
 public class RestaurantRepository implements IRestaurantRepository {
 
-    /**
-     * Guarda la conexion a la base de datos
-     */
     private Connection conn;
 
-    /**
-     * Lista todos los usuarios de la base de datos
-     *
-     * @return Lista de usuarios
-     */
-    @Override
-    public List<User> findAll() {
+    public RestaurantRepository() {
+    }
 
-        List<User> users = new ArrayList<>();
+    @Override
+    public List<Restaurant> findAll() {
+        Restaurant restaurant = null;
+        List<Restaurant> restaurants = new ArrayList<>();
         try {
 
-            String sql = "SELECT id_user, first_name, last_name, address, mobile, email, rol, pws FROM user";
-            this.connect();
+           String sql = "SELECT * from restaurant";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+           
+            ResultSet res = pstmt.executeQuery();
+            if (res.next()) {
+                restaurant = new Restaurant();
+                restaurant.setIdRestaurant(res.getString("idres"));
+                restaurant.setNameRestaurant(res.getString("nameres"));
+                restaurant.setAddressRestaurant(res.getString("addressres"));
+                restaurant.setAddressRestaurant(res.getString("phoneres"));
 
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                User newUser = new User();
-                newUser.setId(rs.getString("id_user"));
-                newUser.setFirstName(rs.getString("first_name"));
-                newUser.setLastName(rs.getString("last_name"));
-                newUser.setAddress(rs.getString("address"));
-                newUser.setMobile(rs.getString("mobile"));
-                newUser.setEmail(rs.getString("email"));
-                newUser.setRol(rs.getString("rol"));
-                newUser.setPws(rs.getString("pws"));
-
-                users.add(newUser);
+                restaurants.add(restaurant);
             }
             this.disconnect();
 
         } catch (SQLException ex) {
             Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return users;
+        return restaurants;
     }
 
-    /**
-     * Busca un usuario de la base de datos
-     *
-     * @param id Identificador del usuario
-     * @return Objeto de tipo usuario
-     */
     @Override
-    public User findById(String id) {
-        User user = null;
+    public Restaurant findById(String id) {
+        Restaurant restaurant = null;
         try {
 
-            String sql = "SELECT id_user, first_name, last_name, address, mobile, email, rol, pws FROM user where id_user=" + id;
-            this.connect();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-
-            if (rs.next()) {
-                user = new User();
-                user.setId(rs.getString("id_user"));
-                user.setFirstName(rs.getString("first_name"));
-                user.setLastName(rs.getString("last_name"));
-                user.setAddress(rs.getString("address"));
-                user.setMobile(rs.getString("mobile"));
-                user.setEmail(rs.getString("email"));
-                user.setRol(rs.getString("rol"));
-                user.setPws(rs.getString("pws"));
+           String sql = "SELECT * from restaurant where id=? ";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, id);
+            ResultSet res = pstmt.executeQuery();
+            if (res.next()) {
+                restaurant = new Restaurant();
+                restaurant.setIdRestaurant(res.getString("idres"));
+                restaurant.setNameRestaurant(res.getString("nameres"));
+                restaurant.setAddressRestaurant(res.getString("addressres"));
+                restaurant.setAddressRestaurant(res.getString("phoneres"));
             }
             this.disconnect();
 
         } catch (SQLException ex) {
-            Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al buscar el usuario en la base de datos", ex);
+            Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al buscar el postre en la base de datos", ex);
         }
-        return user;
+        return restaurant;
     }
 
-    /**
-     * Crea un usuario y lo guarda en la base de datos
-     *
-     * @param newUser Objeto de tipo User a guardar
-     * @return True si puedo crear, false de lo contrario
-     */
     @Override
-    public boolean create(User newUser) {
+    public boolean create(Restaurant restaurant) {
         String sql = "";
         try {
-            this.connect();
-
-            sql = "INSERT INTO user ( id_user, first_name, last_name, address, mobile, email, rol, pws ) "
-                    + "VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )";
-
+             this.connect();
+            sql = "INSERT INTO Restaurant(idRes, nameres,addressres, phoneres,idmenu) VALUES (?,?,?,?,?)";
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, newUser.getId());
-            pstmt.setString(2, newUser.getFirstName());
-            pstmt.setString(3, newUser.getLastName());
-            pstmt.setString(4, newUser.getAddress());
-            pstmt.setString(5, newUser.getMobile());
-            pstmt.setString(6, newUser.getEmail());
-            pstmt.setString(7, newUser.getRol());
-            pstmt.setString(8, newUser.getPws());
+            pstmt.setString(1, restaurant.getIdRestaurant());
+            pstmt.setString(2, restaurant.getNameRestaurant());
+            pstmt.setString(3, restaurant.getAddressRestaurant());
+            pstmt.setString(4, restaurant.getPhone());
+ 
             pstmt.executeUpdate();
+            pstmt.close();
             this.disconnect();
             return true;
         } catch (SQLException ex) {
@@ -131,58 +100,33 @@ public class RestaurantRepository implements IRestaurantRepository {
         return false;
     }
 
-    /**
-     * Actualiza un usuario en la base de datos
-     *
-     * @param newUser Objeto de tipo User a actualizar
-     * @return True si pudo actualizar, false de lo contrario
-     */
     @Override
-    public boolean update(User newUser) {
+    public boolean update(Restaurant newRestaurant) {
         try {
-            this.connect();
-
-            String sql = "UPDATE user "
-                    + "SET first_name = ?, "
-                    + "last_name = ?, "
-                    + "address = ?, "
-                    + "mobile = ?, "
-                    + "email = ?, "
-                    + "rol = ?, "
-                    + "pws = ? "
-                    + "WHERE id_user = ?";
-
+            String sql = "UPDATE restaurant SET nameres=" + "?" + ", addressres=" + "?" + ",phoneres=" + "?" + ", idmenu=" + "?" + "where idres=" + "?";
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, newUser.getFirstName());
-            pstmt.setString(2, newUser.getLastName());
-            pstmt.setString(3, newUser.getAddress());
-            pstmt.setString(4, newUser.getMobile());
-            pstmt.setString(5, newUser.getEmail());
-            pstmt.setString(6, newUser.getRol());
-            pstmt.setString(7, newUser.getPws());
-            pstmt.setString(8, newUser.getId());
+            pstmt.setString(1, newRestaurant.getNameRestaurant());
+            pstmt.setString(2, newRestaurant.getAddressRestaurant());
+            pstmt.setString(3, newRestaurant.getPhone());
+            pstmt.setString(4, newRestaurant.getIdRestaurant());
             pstmt.executeUpdate();
+            pstmt.close();
             this.disconnect();
             return true;
         } catch (SQLException ex) {
-            Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al actualizar el usuario", ex);
+            Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al actualizar el postre", ex);
         }
         return false;
+
     }
 
-    /**
-     * Elimina un usuario de la base de datos
-     *
-     * @param id Identificador del usuario a eliminar
-     * @return True si pudo eliminar, false de lo contrario
-     */
     @Override
     public boolean delete(String id) {
         try {
             this.connect();
 
-            String sql = "DELETE FROM user "
-                    + "WHERE id_user = ?";
+            String sql = "DELETE FROM restaurant "
+                    + "WHERE id_restaurant = ?";
 
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, id);
@@ -190,17 +134,17 @@ public class RestaurantRepository implements IRestaurantRepository {
             this.disconnect();
             return true;
         } catch (SQLException ex) {
-            Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al eliminar el usuario", ex);
+            Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al eliminar postre", ex);
         }
         return false;
     }
 
     /**
-     * Conecta a la bd
+     * Conectar a la bd
      */
     public void connect() {
         try {
-            //Class.forName(Utilities.loadProperty("server.db.driver"));
+//            //Class.forName(Utilities.loadProperty("server.db.driver"));
             //crea una instancia de la controlador de la base de datos
             Utilities ut = new Utilities();
             conn = DriverManager.getConnection(ut.getUrl(), ut.getUsername(), ut.getPwd());
@@ -220,6 +164,6 @@ public class RestaurantRepository implements IRestaurantRepository {
         } catch (SQLException ex) {
             Logger.getLogger(RestaurantRepository.class.getName()).log(Level.SEVERE, "Error al cerrar conexión de la base de datos", ex);
         }
-    }
 
+    }
 }
--- Micro-Service-Restaurant/src/main/java/co/unicauca/restaurant/presentation/rest/RestaurantController.java
+++ Micro-Service-Restaurant/src/main/java/co/unicauca/restaurant/presentation/rest/RestaurantController.java
@@ -1,6 +1,8 @@
 package co.unicauca.restaurant.presentation.rest;
 
-import co.unicauca.common.domain.entity.User;
+
+import co.unicauca.common.domain.entity.Restaurant;
+
 import co.unicauca.restaurant.domain.service.RestaurantService;
 import co.unicauca.restaurant.infra.DomainErrors;
 import co.unicauca.restaurant.infra.JsonResponse;
@@ -24,7 +26,7 @@ import javax.ws.rs.core.Response;
  * @author Camilo Otaya
  */
 @Stateless
-@Path("/users")
+@Path("/restaurants")
 public class RestaurantController {
 
     @Inject
@@ -36,51 +38,46 @@ public class RestaurantController {
 
     /*
         Su uso desde consola mediante client url:
-        curl -X GET http://localhost:8085/Micro-Service-User/user-service/users/ 
+        curl -X GET http://localhost:8085/OnlineRestaurant-Service/restaurant-service/restaurants/ 
 
      */
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public List<User> findAll() {
+    public List<Restaurant> findAll() {
         return service.findAll();
     }
 
     /*
         Su uso desde consola mediante client url:
-        curl -X GET http://localhost:8085/Micro-Service-User/user-service/users/1 
+        curl -X GET http://localhost:8085/OnlineRestaurant-Service/restaurant-service/restaurants/1 
 
      */
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public User findById(@PathParam("id") String id) {
+    public Restaurant findById(@PathParam("id") String id) {
         return service.findById(id);
     }
 
     /*
         Su uso desde consola mediante client url:
         curl -X POST \
-          http://localhost:8085/Micro-Service-User/user-service/users/ \
+          http://localhost:8085/OnlineRestaurant-Service/restaurant-service/restaurants/ \
           -H 'Content-Type: application/json' \
           -d '{
-        "address": "calle 10",
-        "email": "alejo@rc.com",
-        "firstName": "camilo",
-        "id": "40",
-        "lastName": "rodriguez",
-        "mobile": "3166161700",
-        "pws": "123",
-        "rol": "admin"
-    }'
+               "id":1,
+               "name":"Postre de fresa",
+               "price":6000
+        }'
      */
     @POST
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response create(User user) {
+    public Response create(Restaurant restaurant) {
         JsonResponse resp;
-        if (service.create(user)) {
-            resp = new JsonResponse(true, "Usuario creado con éxito", null);
+        if (service.create(restaurant)) {
+            resp = new JsonResponse(true, "Postre creado con éxito", null);
         } else {
-            resp = new JsonResponse(false, "No se pudo crear el usuario", DomainErrors.getErrors());
+            resp = new JsonResponse(false, "No se pudo crear el postre", DomainErrors.getErrors());
         }
         return Response.ok().entity(resp).build();
     }
@@ -88,27 +85,22 @@ public class RestaurantController {
     /*
         Su uso desde consola mediante client url:
         curl -X PUT \
-          http://localhost:8085/Micro-Service-User/user-service/users/40 \
+          http://localhost:8085/OnlineRestaurant-Service/restaurant-service/restaurants/1 \
           -H 'Content-Type: application/json' \
           -d '{
-               "firstName": "Andrés",
-               "lastName": "Valverde",
-               "address": "Calle 26",
-               "mobile": "31232323",
-               "email": "sabastianco@unicauca.edu.co",
-               "rol": "Admin",
-               "pws": "1234"
+               "name":"Postre de fres REF. JDK3-34-343",
+               "price":6000
         }'
      */
     @PUT
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response edit(@PathParam("id") String id, User user) {
+    public Response edit(@PathParam("id") String id, Restaurant restaurant) {
         JsonResponse resp;
-        if (service.update(id, user)) {
-            resp = new JsonResponse(true, "Usuario modificado con éxito", null);
+        if (service.update(id, restaurant)) {
+            resp = new JsonResponse(true, "Postre modificado con éxito", null);
         } else {
-            resp = new JsonResponse(false, "No se pudo modificar el usuario", DomainErrors.getErrors());
+            resp = new JsonResponse(false, "No se pudo modificar el postre", DomainErrors.getErrors());
         }
         return Response.ok().entity(resp).build();
 
@@ -116,7 +108,7 @@ public class RestaurantController {
 
     /*
         Su uso desde consola mediante client url:
-        curl -X DELETE http://localhost:8085/Micro-Service-User/user-service/users/40
+        curl -X DELETE http://localhost:8085/Product-Service/product-service/restaurants/1 
 
      */
     @DELETE
@@ -125,14 +117,15 @@ public class RestaurantController {
         JsonResponse resp;
 
         if (service.delete(id)) {
-            resp = new JsonResponse(true, "Usuario eliminado con éxito", null);
+            resp = new JsonResponse(true, "postre eliminado con éxito", null);
 
         } else {
-            resp = new JsonResponse(false, "No se pudo eliminar el usuario", DomainErrors.getErrors());
+            resp = new JsonResponse(false, "No se pudo eliminar el postre", DomainErrors.getErrors());
         }
         service.delete(id);
 
         return Response.ok().entity(resp).build();
 
     }
+
 }
