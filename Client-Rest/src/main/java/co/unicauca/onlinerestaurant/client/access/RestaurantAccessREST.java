f7a34550ba071158be4289e5b2b2b6986d195ba8
Alejo Rodriguez <alejozeugirdor@unicauca.edu.co>
2020-11-16 15:25:44 -0500

    restaurant client

--- Client-Rest/src/main/java/co/unicauca/onlinerestaurant/client/access/RestaurantAccessREST.java
+++ Client-Rest/src/main/java/co/unicauca/onlinerestaurant/client/access/RestaurantAccessREST.java
@@ -1,27 +1,43 @@
 package co.unicauca.onlinerestaurant.client.access;
 
+import co.unicauca.client.RestaurantJerseyClient;
+import co.unicauca.common.domain.entity.Restaurant;
 import co.unicauca.common.domain.entity.Restaurant;
 import java.util.List;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
 
 /**
  * 
  * @author Santiago Acuña
  */
 public class RestaurantAccessREST implements IRestaurantAccess{
+     RestaurantJerseyClient jersey;
+    Response rta;
 
+    public RestaurantAccessREST() {
+        this.jersey = new RestaurantJerseyClient();
+    }
+
+    /**
+     * Buscar un usuario consumiendo un API REST mediante un cliente jersey
+     *
+     * @param id identificado del usuario
+     * @return objeto usuario
+     * @throws Exception error al buscar un usuario
+     */
     @Override
     public Restaurant findRestaurant(String id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Restaurant user = jersey.findById_JSON(Restaurant.class, id);
+        return user;
     }
-
-    @Override
-    public boolean createRestaurant(Restaurant restaurant) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+  
     @Override
     public List<Restaurant> list() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        GenericType<List<Restaurant>> listResponseTypeM = new GenericType<List<Restaurant>>() {
+        };
+        List<Restaurant> user = jersey.findAll(listResponseTypeM);
+        return user;
     }
 
 }
