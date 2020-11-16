d4368a09f3434e36565ede4ed2e0cd8400adb48e
Alejo Rodriguez <alejozeugirdor@unicauca.edu.co>
2020-11-16 17:37:47 -0500

    GUI Delete fixed

--- Client-Rest/src/main/java/co/unicauca/client/UserJerseyClient.java
+++ Client-Rest/src/main/java/co/unicauca/client/UserJerseyClient.java
@@ -61,13 +61,13 @@ public class UserJerseyClient {
 
     public <T> T findByEmail_XML(Class<T> responseType, String email) throws ClientErrorException {
         WebTarget resource = webTarget;
-        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{email}));
+        resource = resource.path(java.text.MessageFormat.format("email/{0}", new Object[]{email}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
     public <T> T findByEmail_JSON(Class<T> responseType, String email) throws ClientErrorException {
         WebTarget resource = webTarget;
-        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{email}));
+        resource = resource.path(java.text.MessageFormat.format("email/{0}", new Object[]{email}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
