2f5461aac0143ee6ffd389fee62f64c61e8ba094
mariat <mariatrijillca@unicauca.edu.co>
2020-11-17 14:41:10 -0500

    Test DishEntry

--- Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/DishEntryServiceTest.java
+++ Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/DishEntryServiceTest.java
@@ -1,109 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package co.unicauca.dish.domain.service;
 
 import co.unicauca.common.domain.entity.DishEntry;
 import co.unicauca.dish.access.IDishEntryRepository;
+import co.unicauca.dish.access.DishEntryRepository;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Mariat
+ * @author Mariat Trujillo
  */
 public class DishEntryServiceTest {
-    
+
     public DishEntryServiceTest() {
     }
-    /**
-     * Test of findById method, of class DishEntryService.
-     */
-    @Test
-    public void testFindById() {
-        System.out.println("findById");
-        String id = "";
-        DishEntryService instance = new DishEntryService();
-        DishEntry expResult = null;
-        DishEntry result = instance.findById(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of setDishEntryRepository method, of class DishEntryService.
-     */
     @Test
-    public void testSetDishEntryRepository() {
-        System.out.println("setDishEntryRepository");
-        IDishEntryRepository repository = null;
-        DishEntryService instance = new DishEntryService();
-        instance.setDishEntryRepository(repository);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    public void testCrudDishEntrys() {
+        System.out.println("CRUD_DishEntrys");
+        int t;
+        DishEntryService service = new DishEntryService();
+        IDishEntryRepository repo = new DishEntryRepository();
+        service.setDishEntryRepository(repo);
+        service.delete("100");
+        List<DishEntry> tamaño = new ArrayList();
+        tamaño = service.findAll();
+        t = tamaño.size();
+        
+        //crete
+        service.createDishEntry(new DishEntry("100", "dishEntry1", 500));
+        //listarlos
+        List<DishEntry> result = service.findAll();
+        assertEquals(t+1, result.size());
+        //buscar
+        DishEntry dishEntry = service.findById("100");
+        assertEquals("dishEntry1", dishEntry.getNameDishEntry());
+        assertEquals(500, dishEntry.getCostDishEntry());
+        //delete
+        service.delete("100");
+        dishEntry = service.findById("100");
+        assertEquals(null, dishEntry);
 
-    /**
-     * Test of findAll method, of class DishEntryService.
-     */
-    @Test
-    public void testFindAll() {
-        System.out.println("findAll");
-        DishEntryService instance = new DishEntryService();
-        List<DishEntry> expResult = null;
-        List<DishEntry> result = instance.findAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of createDishEntry method, of class DishEntryService.
-     */
-    @Test
-    public void testCreateDishEntry() {
-        System.out.println("createDishEntry");
-        DishEntry newDishEntry = null;
-        DishEntryService instance = new DishEntryService();
-        boolean expResult = false;
-        boolean result = instance.createDishEntry(newDishEntry);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateDishEntry method, of class DishEntryService.
-     */
-    @Test
-    public void testUpdateDishEntry() {
-        System.out.println("updateDishEntry");
-        String id = "";
-        DishEntry newDishEntry = null;
-        DishEntryService instance = new DishEntryService();
-        boolean expResult = false;
-        boolean result = instance.updateDishEntry(id, newDishEntry);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delete method, of class DishEntryService.
-     */
-    @Test
-    public void testDelete() {
-        System.out.println("delete");
-        String id = "";
-        DishEntryService instance = new DishEntryService();
-        boolean expResult = false;
-        boolean result = instance.delete(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
