95bed4c5542c3b72dfc979125af37d1c3078ff5f
mariat <mariatrijillca@unicauca.edu.co>
2020-11-17 14:42:51 -0500

    Test Drink

--- Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/DrinkServiceTest.java
+++ Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/DrinkServiceTest.java
@@ -1,109 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package co.unicauca.dish.domain.service;
 
 import co.unicauca.common.domain.entity.Drink;
 import co.unicauca.dish.access.IDrinkRepository;
+import co.unicauca.dish.access.DrinkRepository;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Mariat
+ * @author Mariat Trujillo
  */
 public class DrinkServiceTest {
     
     public DrinkServiceTest() {
     }
-    /**
-     * Test of findById method, of class DrinkService.
-     */
-    @Test
-    public void testFindById() {
-        System.out.println("findById");
-        String id = "";
-        DrinkService instance = new DrinkService();
-        Drink expResult = null;
-        Drink result = instance.findById(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setDrinkRepository method, of class DrinkService.
-     */
-    @Test
-    public void testSetDrinkRepository() {
-        System.out.println("setDrinkRepository");
-        IDrinkRepository repository = null;
-        DrinkService instance = new DrinkService();
-        instance.setDrinkRepository(repository);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findAll method, of class DrinkService.
-     */
-    @Test
-    public void testFindAll() {
-        System.out.println("findAll");
-        DrinkService instance = new DrinkService();
-        List<Drink> expResult = null;
-        List<Drink> result = instance.findAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of create method, of class DrinkService.
-     */
-    @Test
-    public void testCreate() {
-        System.out.println("create");
-        Drink newDrink = null;
-        DrinkService instance = new DrinkService();
-        boolean expResult = false;
-        boolean result = instance.create(newDrink);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of update method, of class DrinkService.
-     */
-    @Test
-    public void testUpdate() {
-        System.out.println("update");
-        String id = "";
-        Drink newDrink = null;
-        DrinkService instance = new DrinkService();
-        boolean expResult = false;
-        boolean result = instance.update(id, newDrink);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delete method, of class DrinkService.
-     */
-    @Test
-    public void testDelete() {
-        System.out.println("delete");
-        String id = "";
-        DrinkService instance = new DrinkService();
-        boolean expResult = false;
-        boolean result = instance.delete(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
+     @Test
+    public void testCrudDrinks() {
+        System.out.println("CRUD_Drinks");
+        int t;
+        DrinkService service = new DrinkService();
+        IDrinkRepository repo= new DrinkRepository();
+        service.setDrinkRepository(repo);
+        service.delete("100");
+        List<Drink> tamaño=new ArrayList(); 
+        tamaño = service.findAll();
+        t=tamaño.size();
+        
+        //crete
+        service.create(new Drink("100", "drink1", 500d));
+        //listarlos
+        List<Drink> result = service.findAll();
+        assertEquals(t+1, result.size());
+        //buscar
+        Drink drink = service.findById("100");
+        assertEquals("drink1", drink.getNameDrink());
+        assertEquals(500d, drink.getDrinkPrice());
+        //delete
+        service.delete("100");
+        drink = service.findById("100");
+        assertEquals(null, drink);
+
+    }
 }
