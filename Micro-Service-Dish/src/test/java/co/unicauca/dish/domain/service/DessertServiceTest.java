c83d1db0c1f99870ad525d1e803f002cda87093f
mariat <mariatrijillca@unicauca.edu.co>
2020-11-17 14:39:52 -0500

    Test dessert

--- Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/DessertServiceTest.java
+++ Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/DessertServiceTest.java
@@ -1,109 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package co.unicauca.dish.domain.service;
 
 import co.unicauca.common.domain.entity.Dessert;
 import co.unicauca.dish.access.IDessertRepository;
+import co.unicauca.dish.access.DessertRepository;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Mariat
+ * @author Maria Trujillo
  */
 public class DessertServiceTest {
     
-    public DessertServiceTest() {
-    }
-    /**
-     * Test of findById method, of class DessertService.
-     */
-    @Test
-    public void testFindById() {
-        System.out.println("findById");
-        String id = "";
-        DessertService instance = new DessertService();
-        Dessert expResult = null;
-        Dessert result = instance.findById(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+     public DessertServiceTest() {
     }
 
-    /**
-     * Test of setUserRepository method, of class DessertService.
-     */
     @Test
-    public void testSetUserRepository() {
-        System.out.println("setUserRepository");
-        IDessertRepository repository = null;
-        DessertService instance = new DessertService();
-        instance.setUserRepository(repository);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    public void testCrudDesserts() {
+        System.out.println("CRUD_Desserts");
+        int t;
+        DessertService service = new DessertService();
+        IDessertRepository repo= new DessertRepository();
+        service.setUserRepository(repo);
+        service.delete("100");
+        List<Dessert> tamaño=new ArrayList(); 
+        tamaño = service.findAll();
+        t=tamaño.size();
+        
+        //create crea un postre
+        service.create(new Dessert("100", "dessert1", 500d));
+        //list lista todos los postres
+        List<Dessert> result = service.findAll();
+        assertEquals(t+1, result.size());
+        //find busca por id el postre creado
+        Dessert dessert = service.findById("100");
+        assertEquals("dessert1", dessert.getName_Dish_Dessert());
+        assertEquals(500d, dessert.getCost_Dish_Dessert());
+        //delete borra el postre de prueba
+        service.delete("100");
+        dessert = service.findById("100");
+        assertEquals(null, dessert);
 
-    /**
-     * Test of findAll method, of class DessertService.
-     */
-    @Test
-    public void testFindAll() {
-        System.out.println("findAll");
-        DessertService instance = new DessertService();
-        List<Dessert> expResult = null;
-        List<Dessert> result = instance.findAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of create method, of class DessertService.
-     */
-    @Test
-    public void testCreate() {
-        System.out.println("create");
-        Dessert newDessert = null;
-        DessertService instance = new DessertService();
-        boolean expResult = false;
-        boolean result = instance.create(newDessert);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of update method, of class DessertService.
-     */
-    @Test
-    public void testUpdate() {
-        System.out.println("update");
-        String id = "";
-        Dessert newDessert = null;
-        DessertService instance = new DessertService();
-        boolean expResult = false;
-        boolean result = instance.update(id, newDessert);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delete method, of class DessertService.
-     */
-    @Test
-    public void testDelete() {
-        System.out.println("delete");
-        String id = "";
-        DessertService instance = new DessertService();
-        boolean expResult = false;
-        boolean result = instance.delete(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
