02e9d4b80a77851c3e1ed09fa2add095dbd638cb
mariat <mariatrijillca@unicauca.edu.co>
2020-11-17 15:06:09 -0500

    Test Restaurant

--- Micro-Service-Restaurant/src/test/java/co/unicauca/restaurant/domain/service/RestaurantServiceTest.java
+++ Micro-Service-Restaurant/src/test/java/co/unicauca/restaurant/domain/service/RestaurantServiceTest.java
@@ -5,127 +5,94 @@
  */
 package co.unicauca.restaurant.domain.service;
 
-import co.unicauca.common.domain.entity.User;
+import co.unicauca.common.domain.entity.Restaurant;
 import co.unicauca.restaurant.access.IRestaurantRepository;
+import co.unicauca.restaurant.access.RestaurantRepository;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Mariat
+ * @author Maria Trujillo
  */
 public class RestaurantServiceTest {
     
     public RestaurantServiceTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
-    /**
-     * Test of findById method, of class RestaurantService.
+    }  
+   /**
+     * test del CRUD
      */
     @Test
-    public void testFindById() {
-        System.out.println("findById");
-        String id = "";
+    public void testCRUD() {
+        System.out.println("CRUD general");
+        String id = "1000";
+        int t;
+        
+        IRestaurantRepository repo=new RestaurantRepository();
         RestaurantService instance = new RestaurantService();
-        User expResult = null;
-        User result = instance.findById(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setUserRepository method, of class RestaurantService.
-     */
-    @Test
-    public void testSetUserRepository() {
-        System.out.println("setUserRepository");
-        IRestaurantRepository repository = null;
-        RestaurantService instance = new RestaurantService();
-        instance.setUserRepository(repository);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findAll method, of class RestaurantService.
-     */
-    @Test
-    public void testFindAll() {
-        System.out.println("findAll");
-        RestaurantService instance = new RestaurantService();
-        List<User> expResult = null;
-        List<User> result = instance.findAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of create method, of class RestaurantService.
-     */
-    @Test
-    public void testCreate() {
-        System.out.println("create");
-        User newUser = null;
-        RestaurantService instance = new RestaurantService();
-        boolean expResult = false;
-        boolean result = instance.create(newUser);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of update method, of class RestaurantService.
-     */
-    @Test
-    public void testUpdate() {
-        System.out.println("update");
-        String id = "";
-        User newUser = null;
-        RestaurantService instance = new RestaurantService();
-        boolean expResult = false;
-        boolean result = instance.update(id, newUser);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delete method, of class RestaurantService.
-     */
-    @Test
-    public void testDelete() {
-        System.out.println("delete");
-        String id = "";
-        RestaurantService instance = new RestaurantService();
-        boolean expResult = false;
-        boolean result = instance.delete(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Restaurant expResult = new Restaurant();
+        
+        instance.setUserRepository(repo);
+        
+        List<Restaurant> tamaño=new ArrayList(); 
+        tamaño = instance.findAll();
+        t=tamaño.size();
+        
+        expResult.setAddressRestaurant("prueba");
+        expResult.setNameRestaurant("prueba");
+        expResult.setPhone("prueba");
+        expResult.setIdRestaurant(id);
+        expResult.setIdmenuLu("1");
+        expResult.setIdmenuMa("1");
+        expResult.setIdmenuMi("1");
+        expResult.setIdmenuJu("1");
+        expResult.setIdmenuVi("1");
+        expResult.setIdmenuSa("1");
+        //create crear un nuevo resturante
+        
+        instance.create(expResult);
+        
+        //findById buscar el restaurante por id
+        Restaurant result = instance.findById(id);
+        assertEquals("prueba", result.getPhone());      
+        assertEquals("prueba", result.getNameRestaurant());       
+        assertEquals("prueba", result.getAddressRestaurant());       
+        assertEquals("1000", result.getIdRestaurant());
+        
+      
+        //findAll buscar lista de restaurantes
+        List<Restaurant> nresult=new ArrayList(); 
+        nresult = instance.findAll();
+        
+        assertEquals(t+1, nresult.size()); 
+        
+        
+        //Update actualizar el restaurante
+        expResult.setAddressRestaurant("prueba2");
+        expResult.setNameRestaurant("prueba2");
+        expResult.setPhone("prueba2");
+        expResult.setIdRestaurant(id);
+        expResult.setIdmenuLu("1");
+        expResult.setIdmenuMa("1");
+        expResult.setIdmenuMi("1");
+        expResult.setIdmenuJu("1");
+        expResult.setIdmenuVi("1");
+        expResult.setIdmenuSa("1");
+        instance.update(expResult.getIdRestaurant(), expResult);
+        
+        result = instance.findById(id);
+        assertEquals("prueba2", result.getPhone());      
+        assertEquals("prueba2", result.getNameRestaurant());       
+        assertEquals("prueba2", result.getAddressRestaurant());       
+        assertEquals("1000", result.getIdRestaurant());
+        
+        
+        //Delete Borrar el restaurante
+        instance.delete(id);
+        result = instance.findById(id);
+        assertEquals(null, result);           
     }
     
 }
