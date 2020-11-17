9e5221751bb62f5a51a6335cfdea538ac5250294
mariat <mariatrijillca@unicauca.edu.co>
2020-11-17 14:45:24 -0500

    Test MainDish

--- Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/MainDishServiceTest.java
+++ Micro-Service-Dish/src/test/java/co/unicauca/dish/domain/service/MainDishServiceTest.java
@@ -2,6 +2,8 @@ package co.unicauca.dish.domain.service;
 
 import co.unicauca.common.domain.entity.MainDish;
 import co.unicauca.dish.access.IMainDishRepository;
+import co.unicauca.dish.access.MainDishRepository;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -12,98 +14,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Mariat
+ * @author Mariat Trujillo
  */
 public class MainDishServiceTest {
-    
+
     public MainDishServiceTest() {
     }
-    /**
-     * Test of findById method, of class MainDishService.
-     */
-    @Test
-    public void testFindById() {
-        System.out.println("findById");
-        String id = "";
-        MainDishService instance = new MainDishService();
-        MainDish expResult = null;
-        MainDish result = instance.findById(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of setMainDishRepository method, of class MainDishService.
-     */
     @Test
-    public void testSetMainDishRepository() {
-        System.out.println("setMainDishRepository");
-        IMainDishRepository repository = null;
-        MainDishService instance = new MainDishService();
-        instance.setMainDishRepository(repository);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    public void testCrudMainDishs() {
+        System.out.println("CRUD_MainDishes");
+        int t;
+        MainDishService service = new MainDishService();
+        IMainDishRepository repo = new MainDishRepository();
+        service.setMainDishRepository(repo);
+        service.delete("100");
+        List<MainDish> tamaño = new ArrayList();
+        tamaño = service.findAll();
+        t = tamaño.size();
+        
+        //crete
+        service.create(new MainDish("100", "mainDish1", 500d)); 
+        //listarlos
+        List<MainDish> result = service.findAll();
+        assertEquals(t+1, result.size());
+        //buscar
+        MainDish mainDish = service.findById("100");
+        assertEquals("mainDish1", mainDish.getNameDish());
+        assertEquals(500d, mainDish.getDishPrice());
+        //delete
+        service.delete("100");
+        mainDish = service.findById("100");
+        assertEquals(null, mainDish);
 
-    /**
-     * Test of findAll method, of class MainDishService.
-     */
-    @Test
-    public void testFindAll() {
-        System.out.println("findAll");
-        MainDishService instance = new MainDishService();
-        List<MainDish> expResult = null;
-        List<MainDish> result = instance.findAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of create method, of class MainDishService.
-     */
-    @Test
-    public void testCreate() {
-        System.out.println("create");
-        MainDish newMainDish = null;
-        MainDishService instance = new MainDishService();
-        boolean expResult = false;
-        boolean result = instance.create(newMainDish);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of update method, of class MainDishService.
-     */
-    @Test
-    public void testUpdate() {
-        System.out.println("update");
-        String id = "";
-        MainDish newMainDish = null;
-        MainDishService instance = new MainDishService();
-        boolean expResult = false;
-        boolean result = instance.update(id, newMainDish);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delete method, of class MainDishService.
-     */
-    @Test
-    public void testDelete() {
-        System.out.println("delete");
-        String id = "";
-        MainDishService instance = new MainDishService();
-        boolean expResult = false;
-        boolean result = instance.delete(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
